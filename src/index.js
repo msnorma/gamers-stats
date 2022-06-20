0fb56e390ddaf68facafdabf46aea728800ed646
Tiisetso Modubu <tiisetsomodubu@Tiisetsos-MBP.lan>
2022-06-20 18:46:53 +0100

    adding files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './services/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
