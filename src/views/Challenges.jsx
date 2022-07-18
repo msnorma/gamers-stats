953ee828c43cbb902c090fd0f88047d29a13dfde
Tiisetso <msnormaline@gmail.com>
2022-07-18 03:15:45 +0100

    style(dashboard): community dashboard layout

--- src/views/Challenges.jsx
+++ src/views/Challenges.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Card} from 'react-bootstrap';
+import avi from '../assets/black.png';
 
 function Challenges() {
   return (
     <div className="container-content">
-     <h1>Challenges Page</h1>
+    <h3>Challenges</h3>
+     <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src={avi} />
+      <Card.Body>
+        <Card.Title></Card.Title>
+        <Card.Text>
+          Battle Starts
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
     </div>
   );
 }
